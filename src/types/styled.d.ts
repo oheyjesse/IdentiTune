119f10d1d7049b14981236fe69455ce285068bb3
Drew Noll <andrew.noll@acloud.guru>
2021-05-31 23:07:13 +1000

    🐛 Fix typo

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -4,5 +4,5 @@ import { defaultTheme } from 'theme/defaultTheme';
 type ThemeType = typeof defaultTheme;
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType // extends the global DefaultTheme with our ThemeType.
+  export interface DefaultTheme extends ThemeType {} // extends the global DefaultTheme with our ThemeType.
 }
